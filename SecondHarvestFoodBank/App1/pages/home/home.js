74d6b51c1495f2b76d92b1a81b7dd5491edf99d7
Hector Sandoval <hesandova@gmail.com>
2015-04-04 20:30:07 -0700

    Added listview controls to home page still need to wire them to the following pages

--- SecondHarvestFoodBank/App1/pages/home/home.js
+++ SecondHarvestFoodBank/App1/pages/home/home.js
@@ -1,17 +1,21 @@
 ﻿(function () {
     "use strict";
-
+    
     WinJS.UI.Pages.define("/pages/home/home.html", {
         // This function is called whenever a user navigates to this page. It
         // populates the page elements with the app's data.
         ready: function (element, options) {
             // TODO: Initialize the page here.
             WinJS.Utilities.query("a").listen("click", linkClickEventHandler, false);
+
+            var listview = document.getElementById("basic_listview").winControl;
+            listview.addEventListener("iteminvoked", linkClickEventHandler);
         }
     });
     function linkClickEventHandler(eventInfo) {
         eventInfo.preventDefault();
-        var link = eventInfo.target;
-        WinJS.Navigation.navigate(link.href);
+        console.log(eventInfo);
+        //var link = eventInfo.target;
+        //WinJS.Navigation.navigate(link.href);
     }
 })();
