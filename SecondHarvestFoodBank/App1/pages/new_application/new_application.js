a1383d27b7a24191699da6b53dd845868838bcd6
daviGarcia <daviGarcia@csumb.edu>
2015-04-11 13:46:57 -0700

    Initialized variables in the javascript file

--- SecondHarvestFoodBank/App1/pages/new_application/new_application.js
+++ SecondHarvestFoodBank/App1/pages/new_application/new_application.js
@@ -2,7 +2,31 @@
 // http://go.microsoft.com/fwlink/?LinkId=232511
 (function () {
     "use strict";
-
+    var name = '', ssn = '', dob = '', maidenName = '', street = '', 
+city = '', state = '', zipCode = '', mStreet = '', mCity = '', 
+mState = '', mZipCode = '', home = '', work = '', message = '', 
+permAddress = 'no', explain = '', cashAid = 'no', mediCal = 'no', 
+foodStamps = 'no', cams = 'no', otherPrograms = 'no', 
+otherMessage = '', nonCash = 'no', nUsed = '', aidBenefit = '', 
+dReceived = '', counStatCount = '', hispanic = 'no', 
+amerIndian = '', asian = '', filipino = '', cambodian = '', 
+chinese = '', laotian = '', japanese = '', korean = '', 
+vietnamese = '', asianIndian = '', otherAsian = '', 
+nativeHawaiian = '', natHawaiian = '', guamanian = '', samoan = '',
+otherHawaiian = '', engLanguage = '', vietLanguage = '', 
+spanLanguage = '', laoLanguage = '', russLanguage = '', 
+tagLanguage = '', amerSignLanguage = '', cantLanguage = '', 
+cambLanguage = '', otherLanguage = '', migrant = 'no',
+pregnant = 'no', presumptive = 'no', emergencyYes = '', 
+imMedNeed = '', elderAbuse = '', pregnancy = '', childAbuse = '', 
+domAbuse = '', otherEmergency = '', eExplain = '', cash = '', 
+cashText = '', check = '', checkText = '', trust = '', 
+trustText = '', liquidOther = '', liquidText = '', 
+dateIncome1 = '', amountIncome1 = '', dateIncome2 = '', 
+amountIncome2 = '', dateIncome3 = '', amountIncome3 = '', 
+dateIncome4 = '', amountIncome4 = '', rentMortgage = '', 
+utilitiesText = '', eviction = 'no', utilities = 'no', food = 'no',
+clothing = 'no', transportation = 'no';
     WinJS.UI.Pages.define("/pages/new_application.html", {
         // This function is called whenever a user navigates to this page. It
         // populates the page elements with the app's data.
