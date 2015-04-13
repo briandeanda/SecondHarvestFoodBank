e3fcec9dc4d3c724649b4ca792420c692f16dbd2
Hector Sandoval <hesandova@gmail.com>
2015-04-12 20:03:28 -0700

    Added data.js to provide data for the listview in draft_application.html

---
+++ SecondHarvestFoodBank/App1/pages/draft_applications/data.js
@@ -0,0 +1,16 @@
+﻿(function () {
+    "use strict";
+
+
+    var dataList = new WinJS.Binding.List(dataArray);
+
+    // Create a namespace to make the data publicly
+    // accessible. 
+    var publicMembers =
+        {
+            itemList: dataList
+        };
+    WinJS.Namespace.define("DataExample", publicMembers);
+
+
+})();
