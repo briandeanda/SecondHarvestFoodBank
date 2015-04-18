3c02cca11a5403831944b7eb4e6eb8c1d8899a02
daviGarcia <daviGarcia@csumb.edu>
2015-04-17 18:48:53 -0700

    I finished the jQuery code to retreive all the data from the textfields, checkboxes, and radio's.

--- SecondHarvestFoodBank/App1/pages/new_application/new_application.js
+++ SecondHarvestFoodBank/App1/pages/new_application/new_application.js
@@ -6,7 +6,7 @@
 city = '', state = '', zipCode = '', mStreet = '', mCity = '', 
 mState = '', mZipCode = '', home = '', work = '', message = '', 
 permAddress = 'no', explain = '', noHome = '', cashAid = 'no', mediCal = 'no', 
-foodStamps = 'no', cams = 'no', otherPrograms = 'no', 
+foodStamps = 'no', cms = 'no', otherPrograms = 'no', 
 otherMessage = '', nonCash = 'no', nUsed = '', aidBenefit = '', 
 dReceived = '', counStatCount = '', hispanic = 'no', 
 amerIndian = '', asian = '', filipino = '', cambodian = '', 
@@ -33,58 +33,254 @@ clothing = 'no', transportation = 'no';
         ready: function (element, options) {
             // TODO: Initialize the page here.
             $("#name").change(function () {
-                name = $("#name").val();
+                name = $("input[name = 'Name']").val();
             });
             $("#ssn").change(function () {
-                ssn = $("#ssn").val();
+                ssn = $("input[name = 'ssn']").val();
             });
             $("#dob").change(function () {
-                dob = $("#dob").val();
+                dob = $("input[name = 'dob']").val();
             });
             $("#maidenName").change(function () {
-                maidenName = $("#maidenName").val();
+                maidenName = $("input[name = 'maidenName']").val();
             });
             $("#street").change(function () {
-                street = $("#street").val();
+                street = $("input[name = 'street']").val();
             });
             $("#city").change(function () {
-                city = $("#city").val();
+                city = $("input[name = 'city']").val();
             });
             $("#state").change(function () {
-                state = $("#state").val();
+                state = $("input[name = 'state']").val();
             });
             $("#zipCode").change(function () {
-                zipCode = $("#zipCode").val();
+                zipCode = $("input[name = 'zipCode']").val();
             });
             $("#mStreet").change(function () {
-                mStreet = $("#mStreet").val();
+                mStreet = $("input[name = 'mStreet']").val();
             });
             $("#mCity").change(function () {
-                mCity = $("#mCity").val();
+                mCity = $("input[name = 'mCity']").val();
             });
             $("#mState").change(function () {
-                mState = $("#mState").val();
+                mState = $("input[name = 'mState']").val();
             });
             $("#mZipCode").change(function () {
-                mZipCode = $("#mZipCode").val();
+                mZipCode = $("input[name = 'mZipCode']").val();
             });
             $("#home").change(function () {
-                home = $("#home").val();
+                home = $("input[name = 'home']").val();
             });
             $("#work").change(function () {
-                work = $("#work").val();
+                work = $("input[name = 'work']").val();
             });
             $("#message").change(function () {
-                message = $("#message").val();
+                message = $("input[name = 'message']").val();
             });
 
             permAddress = $("input[name = 'permAddress']:checked").val();
 
+            
+			noHome = $("input[name = 'noHome']:checked").val();
             if ($('#noHome').is(":checked")) {
-                noHOme = $('#noHome').val();
-            }
+                noHome = $('#noHome').val();
+				}
+			
+			$("#explainPerm").change(function(){
+				explain = $("input[name = 'explainPerm']").val();
+			})	
+				
+			cashAid = $("input[name = 'cashAid']:checked").val();	
+			mediCal = $("input[name = 'mediCal']:checked").val();	
+			foodStamps = $("input[name = 'foodStamps']:checked").val();
+			cms = $("input[name = 'cms']:checked").val();	
+			otherPrograms = $("input[name = 'otherPrograms']:checked").val();		
+			
+				
+			$("#otherMessage").change(function () {
+                otherMessage = $("input[name = 'otherMessage']").val();
+            });	
+            
+            nonCash = $("input[name = 'nonCash']:checked").val();
+
+			$("#nUsed").change(function () {
+                nUsed = $("input[name = 'nUsed']").val();
+            });		
+            
+            $("#aidBenefit").change(function () {
+                nUsed = $("input[name = 'aidBenefit']").val();
+            });	
+            
+            $("#dReceived").change(function () {
+                nUsed = $("input[name = 'dReceived']").val();
+            });
+            
+            $("#counStatCount").change(function () {
+                counStatCount = $("input[name = 'counStatCount']").val();
+            });
+            
+            hispanic = $("input[name = 'hispanic']:checked").val();
+            
+            amerIndian = $("input[name = 'amerIndian']:checked").val();
+
+			asian = $("input[name = 'asian']:checked").val();
+
+			filipino = $("input[name = 'filipino']:checked").val();
+
+			cambodian = $("input[name = 'cambodian']:checked").val();
+
+			chinese = $("input[name = 'chinese']:checked").val();
+
+			laotian = $("input[name = 'laotian']:checked").val();
+
+			japanese = $("input[name = 'japanese']:checked").val();
+
+			korean = $("input[name = 'korean']:checked").val();
+
+			vietnamese = $("input[name = 'vietnamese']:checked").val();
+
+			asianIndian = $("input[name = 'asianIndian']:checked").val();
+            
+            $("#otherAsian").change(function () {
+                otherAsian = $("input[name = 'otherAsian']").val();
+            });
+            
+            nativeHawaiian = $("input[name = 'nativeHawaiian']:checked").val();
+
+			natHawaiian = $("input[name = 'natHawaiian']:checked").val();
+
+			guamanian = $("input[name = 'guamanian']:checked").val();
+
+			samoan = $("input[name = 'samoan']:checked").val();
+
+            
+            $("#otherHawaiian").change(function () {
+                otherAsian = $("input[name = 'otherHawaiian']").val();
+            });
+
+          	engLanguage = $("input[name = 'engLanguage']:checked").val();
+
+			vietLanguage = $("input[name = 'vietLanguage']:checked").val();
+
+			spanLanguage = $("input[name = 'spanLanguage']:checked").val();
+
+			laoLanguage = $("input[name = 'laoLanguage']:checked").val();
+
+			russLanguage = $("input[name = 'russLanguage']:checked").val();
+
+			amerSignLanguage = $("input[name = 'amerSignLanguage']:checked").val();
+
+			cantLanguage = $("input[name = 'cantLanguage']:checked").val();
+
+			cambLanguage = $("input[name = 'cambLanguage']:checked").val();
+
+			tagLanguage = $("input[name = 'tagLanguage']:checked").val();
+
+			$("#otherLanguage").change(function () {
+			                otherLanguage = $("input[name = 'otherLanguage']").val();
+			            });
+
+            migrant = $("input[name = 'migrant']:checked").val();
+
+            pregnant = $("input[name = 'pregnant']:checked").val();
+
+            presumptive = $("input[name = 'presumptive']:checked").val();
+
+			emergencyYes = $("input[name = 'emergencyYes']:checked").val();
 
 
+			imMedNeed = $("input[name = 'imMedNeed']:checked").val();
+
+			elderAbuse = $("input[name = 'elderAbuse']:checked").val();
+
+			pregnancy = $("input[name = 'pregnancy']:checked").val();
+
+			childAbuse = $("input[name = 'childAbuse']:checked").val();
+
+			domAbuse = $("input[name = 'domAbuse']:checked").val();
+
+			otherEmergency = $("input[name = 'otherEmergency']:checked").val();
+
+			$("#eExplain").change(function () {
+				eExplain = $("input[name = 'eExplain']").val();
+				});
+
+			cash = $("input[name = 'cash']:checked").val();
+
+			$("#cashText").change(function () {
+				cashText = $("input[name = 'cashText']").val();
+				});
+
+			check = $("input[name = 'check']:checked").val();
+			
+			$("#checkText").change(function () {
+				checkText = $("input[name = 'checkText']").val();
+				});
+
+			trust = $("input[name = 'trust']:checked").val();
+			
+			$("#trustText").change(function () {
+				trustText = $("input[name = 'trustText']").val();
+				});
+
+			liquidOther = $("input[name = 'liquidOther']:checked").val();
+
+			$("#liquidText").change(function () {
+				trustText = $("input[name = 'liquidText']").val();
+				});
+
+				$("#dateIncome1").change(function () {
+				dateIncome1 = $("input[name = 'dateIncome1']").val();
+				});
+				
+				$("#amountIncome1").change(function () {
+				amountIncome1 = $("input[name = 'amountIncome1']").val();
+				});
+				
+				$("#dateIncome2").change(function () {
+				dateIncome2 = $("input[name = 'dateIncome2']").val();
+				});
+				
+				$("#amountIncome2").change(function () {
+				amountIncome2 = $("input[name = 'amountIncome2']").val();
+				});
+				
+				$("#dateIncome3").change(function () {
+				dateIncome3 = $("input[name = 'dateIncome3']").val();
+				});
+				
+				$("#amountIncome3").change(function () {
+				amountIncome3 = $("input[name = 'amountIncome3']").val();
+				});
+				
+				$("#dateIncome4").change(function () {
+				dateIncome4 = $("input[name = 'dateIncome4']").val();
+				});
+				
+				$("#amountIncome4").change(function () {
+				amountIncome4 = $("input[name = 'amountIncome4']").val();
+				});
+				
+
+				$("#rentMortgage").change(function () {
+				rentMortgage = $("input[name = 'rentMortgage']").val();
+				});
+				
+				$("#utilitiesText").change(function () {
+				utilitiesText = $("input[name = 'utilitiesText']").val();
+				});
+		
+				
+			eviction = $("input[name = 'eviction']:checked").val();
+
+			utilities = $("input[name = 'utilities']:checked").val();
+
+			food = $("input[name = 'food']:checked").val();
+
+			clothing = $("input[name = 'clothing']:checked").val();
+
+			transportation = $("input[name = 'transportation']:checked").val();
+				
         },
 
         unload: function () {
